ba4023efb46adcfa5a206816a09e879ff7bd55d0
Ruslan <kandibaruslan@gmail.com>
2022-04-21 11:45:18 +0400

    Task 9 (12/21)

--- functions.php
+++ functions.php
@@ -10,3 +10,19 @@ function getPeoples() {
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $data;
 }
+
+// Task 9
+function createTask() {
+    $text = $_POST['text'];
+
+    if(isset($_POST['submit']) && $text != '') {
+        $pdo = new PDO('mysql:host='.MYSQL_SERVER.';dbname='.MYSQL_DBNAME.'', MYSQL_USER, MYSQL_PASS);
+        $sql = 'INSERT INTO tasks (text) VALUES (:text)';
+        $statement = $pdo->prepare($sql);
+        $statement->execute(['text' => $text]);
+
+        header('Location: task_9.php');
+    }
+
+
+}
