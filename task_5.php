62eafe39080b927d83b60da815afc70f127e74c0
Ruslan <kandibaruslan@gmail.com>
2022-04-22 09:16:25 +0400

    Task 5 (8/21)

--- task_5.php
+++ task_5.php
@@ -1,138 +1,117 @@
+<?php
+
+$peoples = [
+    [
+        'name' => 'Sunny',
+        'surname' => 'A.',
+        'speciality' => 'UI/UX Expert',
+        'position' => 'Lead Author',
+        'twitter' => '@myplaneticker',
+        'username' => 'myorange',
+        'image' => 'sunny.png',
+    ],
+    [
+        'name' => 'Joss',
+        'surname' => 'K.',
+        'speciality' => 'ASP>NET Developer',
+        'position' => 'Partner & Contributor',
+        'twitter' => '@atlantez',
+        'username' => 'Walapa',
+        'image' => 'josh.png',
+    ],
+    [
+        'name' => 'Jovanni',
+        'surname' => 'L.',
+        'speciality' => 'PHP Developer',
+        'position' => 'Partner & Contributor',
+        'twitter' => '@lodev09',
+        'username' => 'lodev09',
+        'image' => 'jovanni.png',
+    ],
+    [
+        'name' => 'Roberto',
+        'surname' => 'R.',
+        'speciality' => 'UI/UX Expert',
+        'position' => 'Partner & Contributor',
+        'twitter' => '@sildur',
+        'username' => 'sildur',
+        'image' => 'roberto.png',
+    ],
+];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="utf-8">
-        <title>
-            Подготовительные задания к курсу
-        </title>
-        <meta name="description" content="Chartist.html">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
-        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
-        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
-        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
-        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
-        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+    <meta charset="utf-8">
+    <title>
+        Подготовительные задания к курсу
+    </title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+    <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+</head>
+<body class="mod-bg-1 mod-nav-link ">
+<main id="js-page-content" role="main" class="page-content">
+    <div class="col-md-6">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    Задание
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                </div>
+            </div>
 
-        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
-        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
-        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
-    </head>
-    <body class="mod-bg-1 mod-nav-link ">
-        <main id="js-page-content" role="main" class="page-content">
-            <div class="col-md-12">
-                <div id="panel-1" class="panel">
-                    <div class="panel-hdr">
-                        <h2>
-                            Задание
-                        </h2>
-                        <div class="panel-toolbar">
-                            <button class="btn btn-panel" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
-                            <button class="btn btn-panel" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
-                        </div>
-                    </div>
-                    <div class="panel-container show">
-                        <div class="panel-content">
-                            <div class="panel-tag">
-                                <p>Сформируйте массив данных и выведите полностью альбом.</p>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+                        <?php foreach ($peoples as $people):?>
+
+                            <div class="rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                                <img src="img/demo/authors/<?=$people['image']?>" alt="<?=$people['name']?> <?=$people['surname']?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                                <div class="ml-2 mr-3">
+                                    <h5 class="m-0">
+                                        <?=$people['name']?> <? echo $people['surname']?> (<?=$people['speciality']?>)
+                                        <small class="m-0 fw-300">
+                                            <?=$people['position']?>
+                                        </small>
+                                    </h5>
+                                    <a href="https://twitter.com/<?=$people['twitter']?>" class="text-info fs-sm" target="_blank"><? echo $people['twitter']?></a>
+                                    -
+                                    <a href="https://wrapbootstrap.com/user/<?=$people['username']?>" class="text-info fs-sm" target="_blank" title="Contact <?=$people['name']?>"><i class="fal fa-envelope"></i></a>
+                                </div>
                             </div>
 
-                            <?php
+                        <? endforeach; ?>
 
-                                $images = [
-                                    [
-                                        'image' => 'img/demo/gallery/21.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/21.jpg',
-                                    ],
-                                    [
-                                        'image' => 'img/demo/gallery/22.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/22.jpg',
-                                    ],
-                                    [
-                                        'image' => 'img/demo/gallery/23.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/23.jpg',
-                                    ],
-                                    [
-                                        'image' => 'img/demo/gallery/24.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/24.jpg',
-                                    ],
-                                    [
-                                        'image' => 'img/demo/gallery/25.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/25.jpg',
-                                    ],
-                                    [
-                                        'image' => 'img/demo/gallery/26.jpg',
-                                        'thumb' => 'img/demo/gallery/thumb/26.jpg',
-                                    ],
-                                ];
-
-                            ?>
-
-                            <div id="js-lightgallery">
-                                <?php foreach ($images as $image):?>
-
-                                <a class="" href="<?echo $image['image']?>">
-                                    <img class="img-responsive" src="<?echo $image['thumb']?>" alt="image">
-                                </a>
-
-                                <? endforeach; ?>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        </main>
-        
-
-        <script src="js/vendors.bundle.js"></script>
-        <script src="js/app.bundle.js"></script>
-        <script src="js/miscellaneous/lightgallery/lightgallery.bundle.js"></script>
-        <script>
-            // default list filter
-            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
-            // custom response message
-            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
-            //accordion filter
-            initApp.listFilter($('#js_list_accordion'), $('#js_list_accordion_filter'));
-            // nested list filter
-            initApp.listFilter($('#js_nested_list'), $('#js_nested_list_filter'));
-            //init navigation 
-            initApp.buildNavigation($('#js_nested_list'));
-
-            $(document).ready(function()
-            {
+        </div>
+    </div>
+</main>
 
 
-                var $initScope = $('#js-lightgallery');
-                if ($initScope.length)
-                {
-                    $initScope.justifiedGallery(
-                    {
-                        border: -1,
-                        rowHeight: 150,
-                        margins: 8,
-                        waitThumbnailsLoad: true,
-                        randomize: false,
-                    }).on('jg.complete', function()
-                    {
-                        $initScope.lightGallery(
-                        {
-                            thumbnail: true,
-                            animateThumb: true,
-                            showThumbByDefault: true,
-                        });
-                    });
-                };
-                $initScope.on('onAfterOpen.lg', function(event)
-                {
-                    $('body').addClass("overflow-hidden");
-                });
-                $initScope.on('onCloseAfter.lg', function(event)
-                {
-                    $('body').removeClass("overflow-hidden");
-                });
-            });
-
-        </script>
-    </body>
+<script src="js/vendors.bundle.js"></script>
+<script src="js/app.bundle.js"></script>
+<script>
+    // default list filter
+    initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+    // custom response message
+    initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+</script>
+</body>
 </html>
